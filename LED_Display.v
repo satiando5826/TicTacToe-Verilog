1273ede35c046dc423c2b6e2f1b7f0fd703c41d1
satiando5826 <sathianpornkhamdee@gmail.com>
2016-12-13 17:07:50 +0700

    fix2.0

--- LED_Display.v
+++ LED_Display.v
@@ -1,6 +1,6 @@
-module LED_Display(l, clk, b, x, gend, reset);
+module LED_Display(p1, p2, l, clk, b, x, gend, reset);
 	output [2:1]l;
-	output clk;
+	output clk, p1, p2;
 	//input from xor and button
 	input b, gend, reset;
 	input [2:1]x;
@@ -10,8 +10,11 @@ module LED_Display(l, clk, b, x, gend, reset);
 	and a1(w, b, gend);
 	
 	D_FF d1(clk, 1'b1, w, reset);
-	D_FF p1(l[1], x[1], clk, reset);
-	D_FF p2(l[2], x[2], clk, reset);
+	D_FF p01(l[1], x[1], clk, reset);
+	D_FF p02(l[2], x[2], clk, reset);
+
+	not np1(p1, l[1]);
+	not np2(p2, l[2]);
 endmodule
 
 module D_FF(q, d, clk, reset);
@@ -23,4 +26,4 @@ module D_FF(q, d, clk, reset);
 		q <= 1'b0;
 	else
 		q <= d;
-endmodule
+endmodule 
